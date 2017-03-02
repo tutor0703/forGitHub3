6d58b333216ae0aea47dbda6e1df775b525419da
Andriy Kruglyanko <casw68@yahoo.com>
2017-03-02 12:22:19 +0200

    1
    1

---
+++ forGitHub3/forGitHub3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  forGitHub3
+//
+//  Created by Andriy Kruglyanko on 02.03.17.
+//  Copyright © 2017 tutor0703. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ forGitHub3/forGitHub3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  forGitHub3
+//
+//  Created by Andriy Kruglyanko on 02.03.17.
+//  Copyright © 2017 tutor0703. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
